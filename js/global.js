845b3bb30dfdfb4ecd0726f8696f212ce6bdb87f
Jon Gilmour <jmgilmour88@gmail.com>
2020-09-25 05:38:00 -0400

    using global js to create comment elements

---
+++ js/global.js
@@ -0,0 +1,11 @@
+function addToHead(tag, definition) {
+  const element = document.createElement(tag);
+  for (const [key, value] of Object.entries(definition)) {
+    element[key] = value;
+  }
+  document.getElementsByTagName('head')[0].appendChild(element);
+}
+
+addToHead('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1'});
+addToHead('link', { rel: 'icon', type: 'image/png', href: 'favicon/favicon-32x32.png'});
+addToHead('link', { rel: 'stylesheet', href: 'global-style.css'});
