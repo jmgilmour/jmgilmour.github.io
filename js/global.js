f91d1c2eb8286e4f074f93785c75e2b4e0bd27f8
Jon Gilmour <jmgilmour88@gmail.com>
2020-09-25 06:36:50 -0400

    testing

--- js/global.js
+++ js/global.js
@@ -9,3 +9,10 @@ function addToHead(tag, definition) {
 addToHead('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1'});
 addToHead('link', { rel: 'icon', type: 'image/png', href: 'images/favicon/favicon-32x32.png'});
 addToHead('link', { rel: 'stylesheet', href: 'css/global.css'});
+
+function addBackButton() {
+  const page = window.location.pathname.split("/").pop();
+  if (page === '' || page === 'index.html') return;
+  console.log('>>>> testing exclude index page');
+}
+addBackButton();
