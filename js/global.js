0d028397ec412cc32f95fb7c3d9e52b73151d88d
Jon Gilmour <jmgilmour88@gmail.com>
2020-09-25 09:30:21 -0400

    removing back button

--- js/global.js
+++ js/global.js
@@ -10,33 +10,33 @@ addToHead('meta', { name: 'viewport', content: 'width=device-width, initial-scal
 addToHead('link', { rel: 'icon', type: 'image/png', href: 'images/favicon/favicon-32x32.png'});
 addToHead('link', { rel: 'stylesheet', href: 'css/global.css'});
 
-function addBackButton() {
-  const page = window.location.pathname.split("/").pop();
-  if (page === '' || page === 'index.html') return;
-
-  const div = document.createElement('div');
-  div.classList.add('solid-arrow-left', 'icon-3');
-  Object.assign(div.style, {
-    position: 'absolute',
-    right: '2em',
-    top: '2em',
-    display: 'flex',
-    'align-items': 'center',
-    'justify-content': 'center',
-    'border-radius': '50%',
-    border: 'solid 2px white',
-    padding: '1em',
-    'background-color': 'white',
-    filter: 'invert(1)',
-  });
-  div.addEventListener('click', (event) => {
-    event.stopPropagation();
-    window.history.back();
-  });
-
-  document.body.appendChild(div);
-}
-
-window.addEventListener('load', () => {
-  addBackButton();
-});
+// function addBackButton() {
+//   const page = window.location.pathname.split("/").pop();
+//   if (page === '' || page === 'index.html') return;
+//
+//   const div = document.createElement('div');
+//   div.classList.add('solid-arrow-left', 'icon-3');
+//   Object.assign(div.style, {
+//     position: 'absolute',
+//     right: '2em',
+//     top: '2em',
+//     display: 'flex',
+//     'align-items': 'center',
+//     'justify-content': 'center',
+//     'border-radius': '50%',
+//     border: 'solid 2px white',
+//     padding: '1em',
+//     'background-color': 'white',
+//     filter: 'invert(1)',
+//   });
+//   div.addEventListener('click', (event) => {
+//     event.stopPropagation();
+//     window.history.back();
+//   });
+//
+//   document.body.appendChild(div);
+// }
+//
+// window.addEventListener('load', () => {
+//   addBackButton();
+// });
